0ab946c4d69c7110e03d8f981c74ecdd7a0998ad
Kitsunito <hinoken.htp@gmail.com>
2022-05-20 18:58:06 -0500

    add Planes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+//Components
 import Header from './Components/layout/Header';
 import Main from './Components/layout/Main';
 import Footer from './Components/layout/Footer';
 import WhatsappIcon from './Components/WhatsappIcon';
-import "./styles/main.scss"
 import MobileMenu from './Components/layout/MobileMenu';
+
+//Pages
+import NotFound from './pages/NotFound'
+import Planes from './pages/Planes'
+
+//Style
+import "./styles/main.scss"
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <MobileMenu />
-      <Main />
-      <WhatsappIcon />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <MobileMenu />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/nosotros" />
+          <Route path="/ejercicios" />
+          <Route path="/planes" element={<Planes />}/>
+          <Route path="/futuro-thalamus" />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <WhatsappIcon />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/layout/MobileMenu.js
+++ src/Components/layout/MobileMenu.js
@@ -1,18 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 
 const MobileMenu = () => {
   return (
     <div className="mobileMenu">
       <div className="menu-links">
-        <h2>SOBRE NOSOTROS</h2>
+        <Link to="/nosotros"><h2>SOBRE NOSOTROS</h2></Link>
         <hr />
-        <h2>EJERCICIOS</h2>
+        <Link to="/ejercicios"><h2>EJERCICIOS</h2></Link>
         <hr />
-        <h2>PLANES</h2>
+        <Link to="/planes"><h2>PLANES</h2></Link>
         <hr />
-        <h2>FUTURO <span>THALAMUS</span> </h2>
+        <Link to="/futuro-thalamus"><h2>FUTURO <span>THALAMUS</span> </h2></Link>
       </div>
       <div className="menu-social">
         <FontAwesomeIcon icon={faWhatsapp} />
