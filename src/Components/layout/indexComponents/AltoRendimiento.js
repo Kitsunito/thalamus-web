d27f534efffe3262fd556d7bf783aea232362838
MarcosLabra <mlabra.dev@gmail.com>
2022-05-21 15:22:21 -0300

    agrego funcionalidad a carrousel

--- src/Components/layout/indexComponents/AltoRendimiento.js
+++ src/Components/layout/indexComponents/AltoRendimiento.js
@@ -2,7 +2,7 @@ const AltoRendimiento = () => {
     return (
         <article className="index-section">
             <img src="/images/backGrounds/altoRendimiento.jpg" alt="" />
-            <h2 className="altoRendimientoTitulo">DEPORTISTAS <span>DE ALTO RENDIMIENTO</span> </h2>
+            <h2>DEPORTISTAS <span>DE ALTO RENDIMIENTO</span> </h2>
         </article>
 
     )
--- src/Components/layout/indexComponents/Entrenadores.js
+++ src/Components/layout/indexComponents/Entrenadores.js
@@ -1,10 +1,28 @@
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
 
+const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 const Entrenadores = () => {
   return (
-    <section className="index-entrenadores">
-      <img src="/images/profiles/JavierCamandona.png" alt="" srcset="" />
-      <h3>con <span>Javier</span></h3>
+    <section className="carrousel">
+      <AutoplaySlider
+        play={true}
+        cancelOnInteraction={false} // should stop playing on user interaction
+        interval={1000}
+        bullets={false}
+        fillParent={true}
+      >
+        <div className='carrousel-entrenadores'>
+          <img src="/images/profiles/JavierCamandona.png" alt="" srcset="" />
+          <h3>con <span>JAVIER</span></h3>
+        </div>
+        <div className='carrousel-entrenadores'>
+          <img src="/images/profiles/CandelaMadariaga.png" alt="" srcset="" />
+          <h3>con <span>CANDELA</span></h3>
+        </div>
+      </AutoplaySlider>
     </section>
   )
 }
