88897beb6ceb7b9acec0429fe010bab2a0de91f3
MarcosLabra <mlabra.dev@gmail.com>
2022-06-04 18:34:14 -0300

    agrego deportistas

--- src/Components/layout/indexComponents/deportistasDesktop.js
+++ src/Components/layout/indexComponents/deportistasDesktop.js
@@ -6,25 +6,65 @@ const DeportistasDesktop = () => {
   return (
     <section className="carrouselDesktop">
       <AwesomeSlider
-      bullets={false}
+        bullets={false}
       >
         <div className="carrouselDesktopContainer">
           <div className='perfil perfilDeportista-BG'>
-            <img src="/images/profiles/JavierCamandona.png" alt="" srcset="" />
-            <h3>con <span>JAVIER</span></h3>
+            <img src="/images/profiles/IgnacioMaestroPuch.png" alt="" srcset="" />
+            <h3>junto a <span>IGNACIO</span></h3>
           </div>
           <div className='descripcion'>
-            <p>Con 23 años, egresado del colegio San Francisco. Actualmente esta cursando la carrera de Prof de educación física en la Fac de ed física de tucuman y una diplomatura en entrenamiento y nutrición de manera on line. Desde ya hace 3 años empezo a trabajar como entrenador, lo cual es lo que lo apasiona, y busco seguir aprendiendo. Hace un mes entro a trabajar en <span>Thalamus</span>, algo que quería desde hace un tiempo.</p>
+            <p>Mi nombre es Ignacio Maestro Puch, naci el 13/03/08 y tengo 14.
+              Empecé a los 11 en una escuelita llamada edmofut, donde me vieron de independiente. donde salí goleador del torneo. En 2015 entro a Atlético donde practico un año y medio fútbol y rugby, siguiendo con el primero por pedido del club. En 2018 me fui a Alemania a jugar pero me lesione las dos rodillas y tuve que parar 8 meses.
+              Al mes y medio de volver entro a primera sin tocar reserva,lo que me costo mucho adaptarme ya era un ritmo totalmente distinto. Comienzo a entrenar en Thalamus lo cual me ayuda mucho a mejorar. Este año me convocaron a la selección.
+            </p>
           </div>
         </div>
+
         <div className="carrouselDesktopContainer">
           <div className='perfil perfilDeportista-BG'>
-            <img src="/images/profiles/JavierCamandona.png" alt="" srcset="" />
-            <h3>con <span>JAVIER</span></h3>
+            <img src="/images/profiles/SantosWagner.png" alt="" srcset="" />
+            <h3>junto a <span>SANTOS</span></h3>
           </div>
           <div className='descripcion'>
-            <p>Con 23 años, egresado del colegio San Francisco. Actualmente esta cursando la carrera de Prof de educación física en la Fac de ed física de tucuman y una diplomatura en entrenamiento y nutrición de manera on line. Desde ya hace 3 años empezo a trabajar como entrenador, lo cual es lo que lo apasiona, y busco seguir aprendiendo. Hace un mes entro a trabajar en <span>Thalamus</span>, algo que quería desde hace un tiempo.</p>
+            <p>Mi nombre es Santos Wagner, tengo 16 años y naci el 22/04/2006.
+              Empecé a jugar al rugby a los 5-6 años en Tucumán Rugby. Actualmente juego en M16.
+              Mi objetivo es ganar los torneos qué hay en el año con mi división y mejorar como persona y jugador.
+              Mi sueño es jugar en los pumas y vivir del rugby profesional en el futuro.</p>
           </div>
+        </div>
+
+        <div className="carrouselDesktopContainer">
+          <div className='perfil perfilDeportista-BG'>
+            <img src="/images/profiles/LucianaGarciaGalo.png" alt="" srcset="" />
+            <h3>junto a <span>LUCIANA</span></h3>
+          </div>
+          <div className='descripcion'>
+            <p>Me llamo Luciana Garcia Galo, tengo 16 años y naci el 28/09/2005.
+              Empece a jugar al hockey a los 5 años. Ahora estoy en el club tucuman rugby.
+              Como capitana desde 2018 al 2021, años que tambien salimos campeonas (mejor jugadora en la final-goleadora en 2019)
+              Debute en la primera del club el 17/07/2021
+              A los 12 años me convocaron al seleccionado tucumano sub 12 formando parte de los siguientes hasta el seleccionado mayores. Mi objetivo es salir campeona con el club y mi
+              sueño, jugar un mundial y un juego olimpico</p>
+          </div>
+        </div>
+
+        <div className="carrouselDesktopContainer">
+          <div className='perfil perfilDeportista-BG'>
+            <img src="/images/profiles/JoaquinMecle.png" alt="" srcset="" />
+            <h3>junto a <span>JOAQUIN</span></h3>
+          </div>
+          <div className='descripcion'>
+            <p>Joaquin Mecle
+              Fecha de nacimiento: 16/03/2007. 15 años
+              Jugador de San Martin de Tucuman
+              Puesto 7/9/11
+              Comencé a jugar al fútbol a los 6 años en la escuelita Play 5.
+              Pase por varias escuelas formativas como  Valladares y Unico Sport hasta que en el año 2018 decidí probar en San Martín.
+              Entreno todos los días en el club y 3 veces por semana en el gimnasio con la rutina para mi y que los profes van adecuando de acuerdo a mis avances y necesidades.
+              Mi objetivo es llegar a debutar en primera y entreno todos los dias para logra eso.</p>
+          </div>
+
         </div>
       </AwesomeSlider>
     </section >
---
+++ src/pages/EjerciciosDesktop.js
@@ -0,0 +1,45 @@
+const EjerciciosDesktop = () => {
+    return (
+        <main className="ejercicios">
+            <section className="bg-1 firstSection">
+                <h2>EJERCICIOS</h2>
+                <h3>De fuerza básica de MM.II</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLx2jwbxdaQHSx8RAqsevnbBENGWBS7NQD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+            <section className="bg-2">
+                <h3>Trabajo de estabilidad</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLx2jwbxdaQHRDq6vAycANSVEXg1QlG-P2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+            <section className="bg-1">
+                <h3>Entrenamiento Físico - Cognitivo</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLx2jwbxdaQHQDajyl71RofZe_KBUupwZh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+            <section className="bg-2">
+                <h3>Ejercicios de potencia de MM.II</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLx2jwbxdaQHQhqPWB9iyGwTSG0SGwiwSg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+            <section className="bg-1">
+                <h3>Técnica de carrera</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLx2jwbxdaQHSv_YIU3jIpH7BRECgH7cSe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+            <section className="bg-2">
+                <h3>Movilidad - Flexibilidad</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLx2jwbxdaQHQr-PTJ00QxKaBhJY6iuBVG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+            <section className="bg-1">
+                <h3>Pliometría - Fuerza reactiva</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLx2jwbxdaQHQkMHzyqGoycsUW3THG67KW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+            <section className="bg-2">
+                <h3>Secundarios de MM.II</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLx2jwbxdaQHSA8q5nvRPJ9u8WwE4Ygu0q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+            <section className="bg-1">
+                <h3>Ejercicios secundarios de MM.SS</h3>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLx2jwbxdaQHT_sj4SBzfmd1QwX-iywboL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+        </main>
+    )
+}
+
+export default EjerciciosDesktop
