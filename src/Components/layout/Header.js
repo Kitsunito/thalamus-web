aee9affdd63228b78a4e1b96d73ede3c750bd80f
Kitsunito <hinoken.htp@gmail.com>
2022-06-05 17:20:35 -0500

    add planes and header animation

--- src/Components/layout/Header.js
+++ src/Components/layout/Header.js
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom';
 
+//Animation
+import {motion} from 'framer-motion';
+import { headerVariants, brandNameVariants, logoVariants } from '../../animations';
+
+
 const Header = () => {
     function handleClickLogo() {
         if (document.getElementById('mobileMenu').classList.contains('show')) {
@@ -11,42 +16,37 @@ const Header = () => {
         document.getElementById('mobileMenu').classList.toggle('show');
     }
 
-    if (window.innerWidth < 1200) {
-        return (
-            <header className='headerMobile'>
-                {/*---- Branding ----*/}
-                <div className="brand">
-                    {/*---- Compony's name ----*/}
-                    <Link to="/" onClick={handleClickLogo}>
-                        <img src="/images/branding/TextoLogoThalamus.svg" className="brand-name-text" alt="logo" />
-                        <img src="/images/branding/LogoThalamus.svg" className="brand-logo" alt="logo" />
-                    </Link>
-                </div>
-                {/*---- Menu Icon ----*/}
-                <img src="/images/icons/menuIcon.svg" alt="Menu Icon" className="menuIcon" onClick={handleClickMenu} />
-            </header>
-        )
-    } else {
-        return (
-            <header className='headerDesktop'>
-                {/*---- Branding ----*/}
-                <div className="brand">
-                    {/*---- Compony's name ----*/}
-                    <Link to="/" onClick={handleClickLogo}>
-                        <img src="/images/branding/TextoLogoThalamus.svg" className="brand-name-text" alt="logo" />
-                        <img src="/images/branding/LogoThalamus.svg" className="brand-logo" alt="logo" />
-                    </Link>
-                </div>
-                {/*---- Nav Bar ----*/}
+    return (
+        <motion.header className={window.innerWidth > 1200 ? "headerDesktop" : "headerMobile"}
+            variants={headerVariants}
+            initial="hidden"
+            animate="visible"
+        >
+            {/*---- Branding ----*/}
+            <div className="brand">
+                {/*---- Compony's name ----*/}
+                <Link to="/" onClick={handleClickLogo}>
+                    <motion.img src="/images/branding/TextoLogoThalamus.svg" className="brand-name-text" alt="logo" 
+                        variants={brandNameVariants}
+                    />
+                    <motion.img src="/images/branding/LogoThalamus.svg" className="brand-logo" alt="logo" 
+                        variants={logoVariants}
+                    />
+                </Link>
+            </div>
+            {/*Dependiendo del tamaño mostramos el ícono del menú o el navbar*/}
+            {/*---- Menu Icon ----*/}
+            {window.innerWidth < 1200 ? 
+                <img src="/images/icons/menuIcon.svg" alt="Menu Icon" className="menuIcon" onClick={handleClickMenu} /> :
                 <div className='navBar'>
                     <Link to="/"><h2 >INICIO</h2></Link>
                     <Link to="/nosotros"><h2 >SOBRE <span>NOSOTROS</span></h2></Link>
                     <Link to="/ejercicios"><h2 >EJERCICIOS</h2></Link>
                     <Link to="/planes"><h2 >PLANES</h2></Link>
-                </div>
-            </header>
-        )
-    }
+                </div>}
+            
+        </motion.header>
+    )
 }
 
 export default Header;
