16c0ba4ddf4d6a8a05ce642e4433ba928645e809
MarcosLabra <mlabra.dev@gmail.com>
2022-06-06 13:42:12 -0300

    pongo autoplay para la version desktop de los carrouseles y ajusto margin de rrss en footer

--- src/Components/layout/indexComponents/EntrenadoresDesktop.js
+++ src/Components/layout/indexComponents/EntrenadoresDesktop.js
@@ -1,11 +1,17 @@
 import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import 'react-awesome-slider/dist/styles.css';
 
+const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 const EntrenadoresDesktop = () => {
   return (
     <section className="carrouselDesktop">
-      <AwesomeSlider
+       <AutoplaySlider
+        play={true}
+        cancelOnInteraction={true} // should stop playing on user interaction
+        interval={4000}
+        bullets={false}
       >
         <div className="carrouselDesktopContainer">
           <div className='perfil perfilEntrenador-BG'>
@@ -34,7 +40,7 @@ const EntrenadoresDesktop = () => {
             <p>Con 27 años, nacida San Miguel de Tucumán. Egresada del Colegio El Salvador en 2012 y de la Facultad de Ed. Física de la UNT en 2021. Trabaja en <span>Thalamus</span> desde 2020, y entrena ahí desde sus inicios. Previamente trabajó en Dinamo y  como profe en la escuelita de fútbol del Colegio Boisdron desde 2016 hasta 2020, actualmente en la Academia de Fútbol DELCO y como Dt de dos equipos de fútbol amateur que participan en el Torneo Las Cañas, El Farça y Patronato. Juega al fútbol desde su infancia, pasando por clubes como San Martín, Argentinos del Norte, Uthgra, entre otros. Actualmente se desempeña en fútbol sala en el club All Boys.</p>
           </div>
         </div>
-      </AwesomeSlider>
+        </AutoplaySlider>
     </section >
   )
 }
