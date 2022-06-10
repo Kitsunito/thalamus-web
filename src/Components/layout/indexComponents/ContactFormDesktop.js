d9b0a028dac7dc0517fd8b755d2422836397acdd
Kitsunito <hinoken.htp@gmail.com>
2022-06-10 00:18:41 -0500

    add envio de mail

--- src/Components/layout/indexComponents/ContactFormDesktop.js
+++ src/Components/layout/indexComponents/ContactFormDesktop.js
@@ -1,29 +1,65 @@
 import { motion } from "framer-motion";
 import { title2Variants } from "../../../animations";
 import { useInView } from "react-intersection-observer";
+import { useState } from "react";
 
 
 const ContactFormDesktop = () => {
 
     const { ref, inView } = useInView();
 
+        //-----States-----
+    //Instanciamos un estado para manejar los datos del formulario
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+
+        //-----Functions-----
+    /*Esta función gestiona el contenido de los inputs almacenando los valores en 
+    el estado formData*/
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
         <section ref={ref} className='contactForm'>
             <h2>Contactanos</h2>
             <div className="contactForm-container">
                 <div>
-                    <form action="">
+                    <form name="frmContacto" method="post" action="sendbymail.php">
                         <div className="input">
                             <label htmlFor="name">Nombre</label>
-                            <input type="text" />
+                            <input type="text" 
+                                name="name" 
+                                onChange={handleChange}
+                                value={formData.name}
+                                id="name"
+                            />
                         </div>
                         <div className="input">
                             <label htmlFor="email">Email</label>
-                            <input type="email" />
+                            <input type="email" 
+                                name="email" 
+                                onChange={handleChange}
+                                value={formData.email}
+                                id="email"
+                            />
                         </div>
                         <div className="input">
                             <label htmlFor="message">Mensaje</label>
-                            <textarea type="text" cols="30" rows="10" ></textarea>
+                            <textarea type="text" 
+                                cols="30" 
+                                rows="10" 
+                                name="message" 
+                                onChange={handleChange}
+                                value={formData.message}
+                                id="message" 
+                            />
                         </div>
                         <button type="submit">ENVIAR</button>
                     </form>
--- src/Components/layout/indexComponents/ContactFormMobile.js
+++ src/Components/layout/indexComponents/ContactFormMobile.js
@@ -1,11 +1,36 @@
+import { useState } from "react";
 const ContactFormMobile = () => {
+
+        //-----States-----
+    //Instanciamos un estado para manejar los datos del formulario
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+
+        //-----Functions-----
+    /*Esta función gestiona el contenido de los inputs almacenando los valores en 
+    el estado formData*/
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
         <section className='contactForm'>
             <h2>Contactanos</h2>
             <form action="">
                 <div className="input">
                     <label htmlFor="name">Nombre</label>
-                    <input type="text" />
+                    <input type="text" 
+                        name="name" 
+                        onChange={handleChange}
+                        value={formData.name}
+                        id="name"
+                    />
                 </div>
                 <div className="input">
                     <label htmlFor="email">Email</label>
@@ -13,7 +38,14 @@ const ContactFormMobile = () => {
                 </div>
                 <div className="input">
                     <label htmlFor="message">Mensaje</label>
-                    <textarea type="text" cols="30" rows="10" ></textarea>
+                    <textarea type="text" 
+                        cols="30" 
+                        rows="10" 
+                        name="message" 
+                        onChange={handleChange}
+                        value={formData.message}
+                        id="message"
+                    />
                 </div>
                 <button type="submit">ENVIAR</button>
             </form>
